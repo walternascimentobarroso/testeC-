0c950c88fa9ba895e78c7edea937f9eb2cf083d9
Walter Nascimento <walter.nascimento.barroso@gmail.com>
2018-05-18 19:54:34 -0400

    [ADJUST] SQL Base
    Signed-off-by: Walter Nascimento <walter.nascimento.barroso@gmail.com>

--- atividade0/script-bd.sql
+++ atividade0/script-bd.sql
@@ -1,104 +1,50 @@
 CREATE DATABASE oficina;
 
 CREATE TABLE especialidade (
-  `id` INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `descricao` VARCHAR(255) 
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  descricao VARCHAR(255) 
 );
 
-subservico
- id
- descricao
- especialidadeId
+CREATE TABLE subservico (
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  descricao VARCHAR(255),
+  especialidadeid INT, 
+  FOREIGN KEY (especialidadeid) REFERENCES especialidade(id)
+);
 
-mao de obra
- id
- codigo
- descricao
- preco-hora
+CREATE TABLE maodeobra (
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  codigo VARCHAR(55),
+  descricao VARCHAR(255),
+  precohora VARCHAR(55)
+);
 
-funcionario
- id
- nome
- maodeobraid
+CREATE TABLE funcionario (
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  nome VARCHAR(255),
+  maodeobraid INT, 
+  FOREIGN KEY (maodeobraid) REFERENCES maodeobra(id)
+);
 
-cliente
- id
- nome
- telefone
- celular
+CREATE TABLE cliente (
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  nome VARCHAR(255),
+  telefone VARCHAR(255),
+  celular VARCHAR(255)
+);
 
-ordemdeservico
- id
- descricao
- clienteId
- funcionarioId
- subservicoId
- descontoId
- observacao
+CREATE TABLE desconto (
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  precototal VARCHAR(255),
+  desconto VARCHAR(255)
+);
 
-desconto
- id
- preco_total
- desconto
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
-
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
--- -----------------------------------------------------
--- Schema estoque
--- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `estoque` ;
-
--- -----------------------------------------------------
--- Schema estoque
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `estoque` DEFAULT CHARACTER SET utf8 ;
-USE `estoque` ;
-
--- -----------------------------------------------------
--- Table `estoque`.`categoria`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `estoque`.`categoria` ;
-
-CREATE TABLE IF NOT EXISTS `estoque`.`categoria` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `nome` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 6
-DEFAULT CHARACTER SET = utf8
-COMMENT = '		';
-
-
--- -----------------------------------------------------
--- Table `estoque`.`produto`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `estoque`.`produto` ;
-
-CREATE TABLE IF NOT EXISTS `estoque`.`produto` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `nome` VARCHAR(45) NOT NULL,
-  `descricao` LONGTEXT NOT NULL,
-  `preco_custo` DECIMAL(9,2) NOT NULL,
-  `preco_venda` DECIMAL(9,2) NOT NULL,
-  `quantidade` DOUBLE NOT NULL,
-  `unidade_medida` CHAR(2) NULL DEFAULT NULL,
-  `categoria_id` INT(11) NOT NULL,
-  PRIMARY KEY (`id`, `categoria_id`),
-  INDEX `fk_produto_categoria_idx` (`categoria_id` ASC),
-  CONSTRAINT `fk_produto_categoria`
-    FOREIGN KEY (`categoria_id`)
-    REFERENCES `estoque`.`categoria` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-AUTO_INCREMENT = 12
-DEFAULT CHARACTER SET = utf8;
-
-
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+CREATE TABLE ordemdeservico (
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  descricao VARCHAR(255),
+  clienteid INT, FOREIGN KEY (clienteid) REFERENCES cliente(id),
+  funcionarioid INT, FOREIGN KEY (funcionarioid) REFERENCES funcionario(id),
+  subservicoid INT, FOREIGN KEY (subservicoid) REFERENCES subservico(id),
+  descontoid INT, FOREIGN KEY (descontoid) REFERENCES desconto(id),
+  observacao VARCHAR(255)
+);
